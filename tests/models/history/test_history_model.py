577bce6709ccfe59246f4ff137eef40fa50f1666
Bruno Ferreira <contatobrunokye@gmail.com>
2023-08-14 18:19:59 -0300

    feat: requisito 07

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,23 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    list = HistoryModel.list_as_json()
+    data = json.loads(list)
+
+    data[0].pop("_id")
+    data[1].pop("_id")
+
+    assert data[0] == {
+        "text_to_translate": "Hello, I like videogame",
+        "translate_from": "en",
+        "translate_to": "pt",
+    }
+
+    assert data[1] == {
+        "text_to_translate": "Do you love music?",
+        "translate_from": "en",
+        "translate_to": "pt",
+    }
